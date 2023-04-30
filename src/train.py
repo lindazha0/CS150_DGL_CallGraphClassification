1cc2a9c96f9a4134275ac29eb27b28e333d25a10
Linda Zhao <czhao07@login-dev-01.pax.tufts.edu>
2023-04-30 17:42:41 -0400

    fit train.py with similarities

--- src/train.py
+++ src/train.py
@@ -3,11 +3,39 @@ from torch_geometric.loader import DataLoader
 import torch
 import copy
 
-#TODO: please import your gnn from gnn.py
-from gnn import GNN
-from sklearn.model_selection import train_test_split
+from model import GNN
+from sklearn.model_selection import train_test_split, mean_squared_error
+from direct_graph_edit_dis import graph_edit_distance
+from earth_move_dis import wasserstein_distance
 
-#TODO: please implement a function that checks the accuracy of the validation set
+def graph_similarities(graphs):
+    """
+    Compute graph similarities between all pairs of graphs, return a similarity matrix
+    args:
+        graphs: a list of graphs
+    """
+    res = []
+    for i in range(len(graphs)):
+        for j in range(i+1, len(graphs)):
+            if i == j:
+                continue
+            res.append(graph_edit_distance(graphs[i], graphs[j]))
+    return res
+
+def embedding_similarities(embeddings):
+    """
+    Compute embedding similarities between all pairs of embeddings, return a similarity matrix
+    args:
+        embeddings: a list of embeddings
+    """
+    res = []
+    for i in range(len(embeddings)):
+        for j in range(i+1, len(embeddings)):
+            if i == j:
+                continue
+            res.append(wasserstein_distance(embeddings[i], embeddings[j]))
+    return res
+
 def validate(model, testset):
     """
     Validate the model by checking its accuracy on the validation set of nodes
@@ -21,13 +49,13 @@ def validate(model, testset):
 
     test_loader = DataLoader(testset, batch_size=len(testset), shuffle=False)
     for batch in test_loader:
-        out = model(batch.x, batch.edge_index, batch.batch)
-        pred = out.argmax(dim=1)
-        acc = pred.eq(batch.y).sum().item() / batch.y.size(0)
+        embeddings = model(batch.x, batch.edge_index, batch.batch)
+        ground_truth = graph_similarities(batch.to_data_list())
+        similar_mat = embedding_similarities(embeddings)
+        err = mean_squared_error(similar_mat, ground_truth)
 
-    return acc
+    return err
 
-#TODO: please implement a function that trains model 
 def train(trainset):
     """
     A training function that trains a GNN model 
@@ -45,15 +73,15 @@ def train(trainset):
     # NOTE: please use a loalder to form training batches
     train_loader = DataLoader(trainset, batch_size=4, shuffle=True)
 
-    # TODO: please implement a training loop to train a model 
-    best_val_acc = 0.0
+    # training loop to train a model 
+    min_val_err = 100
     best_model = None
     epochs = 100
-    criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
+    # train the model
     for epoch in range(epochs):
-        # train the model
         for batch in train_loader:
             model.train()
 
@@ -61,21 +89,23 @@ def train(trainset):
             optimizer.zero_grad()
 
             # forward pass
-            out = model(batch.x, batch.edge_index, batch.batch)
-            loss = criterion(out, batch.y)
+            embeddings = model(batch.x, batch.edge_index, batch.batch)
+            ground_truth = graph_similarities(batch.to_data_list())
+            similar_mat = embedding_similarities(embeddings)
+            loss = criterion(similar_mat, ground_truth)
             loss.backward()
             optimizer.step()
         # print the loss
         print('Epoch: {:03d}, Loss: {:.5f}'.format(epoch, loss.item()))
 
         # validate the model
-        val_acc = validate(model, valset)
+        val_err = validate(model, valset)
 
         # save the best model
-        if val_acc > best_val_acc:
-            best_val_acc = val_acc
+        if val_err < min_val_err:
+            min_val_err = val_err
             best_model = copy.deepcopy(model)
-    print('Best validation accuracy: {:.5f}'.format(best_val_acc))
+    print('Minimal validation error: {:.5f}'.format(min_val_err))
 
     return best_model
 
