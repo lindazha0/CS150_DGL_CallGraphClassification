d6212b687c08d4278c977ffb508bd5a05102dcc1
Linda Zhao <czhao07@login-dev-01.pax.tufts.edu>
2023-04-30 17:41:32 -0400

    encapsulate dist functions

--- src/direct_graph_edit_dis.py
+++ src/direct_graph_edit_dis.py
@@ -3,11 +3,18 @@
 # nx function might not fit directed graphs
 import networkx as nx
 
-# Create directed graphs using NetworkX
-G1 = nx.DiGraph([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 1)])
-G2 = nx.DiGraph([(0, 1), (1, 2), (2, 3), (1, 4)])
+def graph_edit_distance(G1, G2):
+    return nx.graph_edit_distance(G1, G2)
+
+def main():
+    # Create directed graphs using NetworkX
+    G1 = nx.DiGraph([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 1)])
+    G2 = nx.DiGraph([(0, 1), (1, 2), (2, 3), (1, 4)])
 
 
-# Compute graph edit distance
-ged = nx.graph_edit_distance(G1, G2)
-print("Graph Edit Distance:", ged)
+    # Compute graph edit distance
+    ged = nx.graph_edit_distance(G1, G2)
+    print("Graph Edit Distance:", ged)
+
+if __name__ == "__main__":
+    main()
--- src/earth_move_dis.py
+++ src/earth_move_dis.py
@@ -1,8 +1,16 @@
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.wasserstein_distance.html
 from scipy.stats import wasserstein_distance
 
-V1 = [0, 1, 3]
-V2 = [5, 6, 8]
+def wasserstein_distance(V1, V2):
+    return wasserstein_distance(V1, V2)
 
-dist = wasserstein_distance(V1, V2)
-print(f"Wasserstein Distance: {dist}")
+def main():
+    V1 = [0, 1, 3]
+    V2 = [5, 6, 8]
+
+    dist = wasserstein_distance(V1, V2)
+    print(f"Wasserstein Distance: {dist}")
+    return
+
+if __name__ == "__main__":
+    main()
